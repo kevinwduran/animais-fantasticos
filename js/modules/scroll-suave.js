b88400bf77c9d3bd531ad537525850d67c3b3e9a
Kevin Duran <feneco777@gmail.com>
2023-12-29 02:09:08 -0300

    Comentário scroll

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,7 +1,8 @@
 export default class ScrollSuave {
+  //é possível passar o link e opções desejadas, se necessário.
   constructor(links, options) {
-    this.linksInternos = document.querySelectorAll(links);
-    if (options === undefined) {
+    this.linksInternos = document.querySelectorAll(links);//pega todos os links internos
+    if (options === undefined) { //se opções forem undefined, aplique o padrão
       this.options = {
         behavior: 'smooth',
         block: 'start',
@@ -9,15 +10,15 @@ export default class ScrollSuave {
     } else {
       this.options = options;
     }
-
+    //this da class ScrollSuave
     this.scrollToSection = this.scrollToSection.bind(this);
   }
 
-  scrollToSection(event) {
+  scrollToSection(event) { //link
     event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView(this.options);
+    const href = event.currentTarget.getAttribute('href'); // pega o '#exemplo'
+    const section = document.querySelector(href); //pega a section referente ao href
+    section.scrollIntoView(this.options); //método js que vai até o elemento desejado, neste caso, a section
   }
 
   addLinkEvent() {
