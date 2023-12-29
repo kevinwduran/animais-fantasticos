7aa2d584e303d05c8264732eca0a1e2858e1e7be
Kevin Duran <feneco777@gmail.com>
2023-12-28 22:42:28 -0300

    TabNav refatorado

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -1,21 +1,32 @@
-export default function initTabNav() {
-  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-  const tabContent = document.querySelectorAll('[data-tab="content"] section');
-
-  function activeTab(index) {
-    tabContent.forEach((section) => {
-      section.classList.remove('ativo');
-    });
-    const direcao = tabContent[index].dataset.anime;
-    tabContent[index].classList.add('ativo', direcao);
+//Mudança do conteúdo ao clique
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabMenu = document.querySelectorAll(menu); //imagens animais
+    this.tabContent = document.querySelectorAll(content); //conteúdo referente a cada animal
+    this.activeClass = 'ativo';
   }
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener('click', () => {
-        activeTab(index);
-      });
+  //ativa a tab de acordo com o index da mesma
+  activeTab(index) {
+    this.tabContent.forEach((sectionConteudo) => {
+      sectionConteudo.classList.remove(this.activeClass);
     });
+    const direcao = this.tabContent[index].dataset.anime; //data-anime="show-right"
+    this.tabContent[index].classList.add(this.activeClass, direcao); //a direção é passada aqui para ter a transição, apenas.
+  }
+
+  //adiciona os eventos as tabs
+  addTabNavEvent() {
+    this.tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
+  }
+
+  init() {
+    if (this.tabMenu.length && this.tabContent.length) {
+      //ativar primeiro item sempre que a página for carregada
+      this.activeTab(0);
+      this.addTabNavEvent();
+    }
   }
 }
