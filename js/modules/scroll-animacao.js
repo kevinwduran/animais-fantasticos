317ff89c304d5dd83606e3328002566d8bbbbc04
Kevin Duran <feneco777@gmail.com>
2023-12-29 20:48:20 -0300

    Animação ao Scroll Feita

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -2,25 +2,51 @@ export default class ScrollAnimacao {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.classAtivo = 'ativo';
+    //animação acontece quando se passa o scroll na metade da tela
     this.windowMetade = window.innerHeight * 0.6;
-
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checarDistancia = this.checarDistancia.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (isSectionVisible) {
-        section.classList.add(this.classAtivo);
-      } else if (section.classList.contains(this.classAtivo)) {
-        section.classList.remove(this.classAtivo);
+  //pega a distância em cada objeto em relação ao topo do site
+  getDistancia() {
+    //forEach não tem return, por isso usamos o map
+    this.distance = [...this.sections].map((section) => { //desestruturado para usar o map
+      //Distância de cada section referente ao topp
+      const sectionTop = section.offsetTop;
+      //retorna o elemento (section) e sua distância até o topo
+      return {
+        elemento: section,
+        offsetTop: Math.floor(sectionTop - this.windowMetade),
+      };
+    });
+  }
+
+  //chegar distância em cada objeto em relação ao scroll do site
+  checarDistancia() {
+    this.distance.forEach((section) => {
+      //se valor do scroll até o topo for maior que o valor da section até o topo
+      if (window.scrollY > section.offsetTop) {
+        //cada section, seu elemento, adiciona ou remove a classe
+        section.elemento.classList.add(this.classAtivo);
+      } else if (section.elemento.classList.contains(this.classAtivo)) {
+        section.elemento.classList.remove(this.classAtivo);
       }
     });
   }
 
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistancia();
+      //já chama o método checarDistancia para rodar uma vez para a primeira section acontecer
+      //e não esperar chegar na metade da tela
+      this.checarDistancia();
+      window.addEventListener('scroll', this.checarDistancia);
+    }
+    return this;
+  }
+
+  //se precisar parar a função, apenas coloque sua const, let .stopScrollAnimation()
+  stopScrollAnimation() {
+    window.removeEventListener('scroll', this.checarDistancia);
   }
 }
